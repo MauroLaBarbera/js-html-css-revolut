8b2054b364df65a9d22c5a73fc91aa8a3f17f56f
MauroLaBarbera <you@example.com>
2021-04-11 14:08:39 +0200

    finito

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
-    console.log('ciao');
-
     
+    /**
+     * DROPDOWN MENU
+     */
+    //REFERENZE
+    var dropDown = $('.with-dropdown');
+    var dropLinks = dropDown.children('a');
+    var dropMenus = dropDown.children('.dropdown-menu');
+
+    //CLICK MOSTRA/NASCONDI MENU'
+    dropLinks.click(function(e) {
+
+
+        var actualMenu = $(this).next('.dropdown-menu');
+        //RESET
+        dropMenus.not(actualMenu).hide();
+
+        actualMenu.toggle();
+    }); 
+
+
+
+
+    //FINE CODICE
 });
